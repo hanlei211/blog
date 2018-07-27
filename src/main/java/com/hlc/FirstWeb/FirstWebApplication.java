e52620ec1b82c0faf16ca8e03192d02cdcd5003d
hanlei <463779213@qq.com>
2018-07-27 15:15:52 +0800

    初始建springboot

---
+++ src/main/java/com/hlc/FirstWeb/FirstWebApplication.java
@@ -0,0 +1,16 @@
+package com.hlc.FirstWeb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@MapperScan("com.hlc.FirstWeb.model")
+public class FirstWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstWebApplication.class, args);
+    }
+}
